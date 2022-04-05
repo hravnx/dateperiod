8ce093e65ff7f3d38d8e49b2cd1f0a67d1cd3720
Henrik Ravn <hravnx@gmail.com>
2022-04-05 06:46:28 +0200

    WiP on build script

--- build.fsx
+++ build.fsx
@@ -1,5 +1,6 @@
 ﻿#r "nuget: FsMake"
 #r "nuget: DotEnv.Net"
+#R "nuget: Nuget.Core"
 
 open FsMake
 open System
@@ -20,29 +21,18 @@ let NUGET_APIKEY = EnvVar.getOrFail "NUGET_APIKEY"
 
 // STEPS --------------------------------------------------------------
 
-let cleanBuildOutput () =
+let cleanAndRestore = Step.create "cleanAndRestore" {
     [   "src/lib/obj";   "src/lib/bin"
         "test/unit/obj"; "test/unit/obj"
         ".pack" 
     ]
     |> Seq.iter (fun x -> if Directory.Exists x then Directory.Delete(x, true))
-
-let clean = Step.create "clean" {
-    cleanBuildOutput ()
-}
-
-let restore = Step.create "restore" {
-    do! dotnet ["restore"]
-        |> Cmd.run
+    do! dotnet ["restore"] |> Cmd.run
 }
 
 let build = Step.create "build" {
     let! ctx = Step.context
     let config = configFrom ctx
-    if config = "Release" then
-        cleanBuildOutput ()
-        do! dotnet ["restore"]
-            |> Cmd.run
         
     do! dotnet ["build"; "-c"; config]
         |> Cmd.args ["--no-restore"]
@@ -59,7 +49,7 @@ let unitTest = Step.create "test:unit" {
         |> Cmd.run
 }
 
-let checkClean = Step.create "git:check" {
+let verifyRepoIsClean = Step.create "git:status" {
     let! statusOut = 
         Cmd.createWithArgs "git" [ "status"; "-s" ]
         |> Cmd.redirectOutput Cmd.Redirect
@@ -69,8 +59,7 @@ let checkClean = Step.create "git:check" {
         do! Step.fail "Repo is not clean, release aborted"
 }
 
-
-let pack = Step.create "nuget:pack" {
+let nugetPack = Step.create "nuget:pack" {
     let! ctx = Step.context
     let config = configFrom ctx
     do! dotnet ["pack"; "-c"; config]
@@ -78,7 +67,7 @@ let pack = Step.create "nuget:pack" {
         |> Cmd.run
 }
 
-let push = Step.create "nuget:push" {
+let nugetPush = Step.create "nuget:push" {
     let! ctx = Step.context
     let! nugetAPIKey = NUGET_APIKEY
     ctx.Console.WriteLine (Console.warn $"API KEY {nugetAPIKey}")
@@ -87,29 +76,28 @@ let push = Step.create "nuget:push" {
 // PIPELINES ----------------------------------------------------------
 
 Pipelines.create {
-    do! 
-        Pipeline.create "clean" {
-            run clean
-        }
+    do! Pipeline.create "clean" {
+        run cleanAndRestore
+    }
 
-    let! build =
-        Pipeline.create "build" {
-            run restore
-            run build
-        }
+    do! Pipeline.create "test" {
+        run build
+        run unitTest
+    }
 
-    let! test = 
-        Pipeline.createFrom build "test" {
-            run unitTest
-        }
+    do! Pipeline.create "release" {
+        run verifyRepoIsClean
+        run cleanAndRestore
+        run build
+        run unitTest
+        run nugetPack
+        run nugetPush
+    }
 
-    do!
-        Pipeline.createFrom test "release" {
-            run checkClean
-            run pack
-            run push
-        }
+    let! defaultPipeline = Pipeline.create "build" {
+        run build
+    }
 
-    default_pipeline build
+    default_pipeline defaultPipeline
 }
 |> Pipelines.runWithArgsAndExit fsi.CommandLineArgs
